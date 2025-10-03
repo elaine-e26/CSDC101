febfe0a5755b19e5990bc1c6398c81548e31fd6d
Elaine <eeguia@gbox.adnu.edu.ph>
2025-10-02 21:54:34 -0700

    Create While.cpp

---
+++ LOOPS/While.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number = 0;
+    
+    while(number <= 5){ //condition
+        cout<< "Likey number: "<< number << endl;
+        number++; //can be any operator
+        
+    }
+    return 0;
+}
