19b68d5e1cbeeea7b22fb64a1c479b627ba11e60
Elaine <eeguia@gbox.adnu.edu.ph>
2025-10-02 21:55:26 -0700

    Create DOWHILE.cpp

---
+++ LOOPS/DOWHILE.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char answer;
+    
+    do{
+        cout<< "Do you want a build a snowman? Press y for yes and n for no" <<endl;
+        cin>> answer;
+        cout<< "Sing let it go" <<endl;
+    }
+    while(answer == y); //condition
+     
+     cout<< "okay byeeeee" << endl;
+        
+    
+    return 0;
+}
