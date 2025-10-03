817b039785b5af6627c23caf6f0b2bfdf44c224d
Elaine <eeguia@gbox.adnu.edu.ph>
2025-10-02 21:56:36 -0700

    Create FOR.cpp

---
+++ LOOPS/FOR.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    for(int start = 0; start < 5; start++){ //++ is increment, -- is decrement
+        cout<< "likey number: "<< start << endl;
+        
+    }
+        
+    
+    return 0;
+}
