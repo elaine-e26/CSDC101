f0a84fef9b46cab37626d3c0e1d972f5925b0ad8
Elaine <eeguia@gbox.adnu.edu.ph>
2025-10-20 10:08:11 +0800

    Create SumOfNaturalNumbers.cpp

---
+++ Loop Practice/SumOfNaturalNumbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, sum = 0;
+
+    cout << "Enter a number: ";
+    cin >> num;
+  
+    for (int i = 1; i <= num; i++) {
+        sum += i; // sum + i = i
+    }
+    cout << "Sum = " << sum << endl;
+  
+    return 0;
+}
