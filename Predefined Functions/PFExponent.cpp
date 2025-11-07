a812a1c51027155a732ae55843461ce162bb2977
Elaine <eeguia@gbox.adnu.edu.ph>
2025-11-07 12:20:29 +0800

    Create PFExponent.cpp

---
+++ Predefined Functions/PFExponent.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath> 
+
+using namespace std;
+
+int main (){
+   int n = 3, exponent = 2;
+  cout<< n << " raise to " << exponent << " is equals to " << pow(n, exponent) << endl; //pow = 3^2
+
+return 0;
+}
