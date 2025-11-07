7f76db505358aa709155399dc3873c2d3b4e9187
Elaine <eeguia@gbox.adnu.edu.ph>
2025-11-07 13:35:09 +0800

    Create DefinitionBeforeBasicGreeting.cpp

---
+++ User Defined Functions/DefinitionBeforeBasicGreeting.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void GoodMorning(){     //void = function without return value
+  cout << "Hi! Good morning!" << endl;
+}
+
+void GoodEvening(){
+  cout << "Hello! Good evening!" << endl;
+}
+
+int main(){
+
+  GoodMorning();
+  GoodEvening();
+
+return 0;
+}
