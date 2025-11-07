50508f5ee9eeba58e33839f488ab4f92fe950495
Elaine <eeguia@gbox.adnu.edu.ph>
2025-11-07 12:33:55 +0800

    Create TLTime.cpp

---
+++ Predefined Functions/TLTime.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <ctime> //library for time functions
+
+using namespace std;
+
+int main (){
+  
+  cout << "The current time in seconds is " << time(0)<< endl; //current time since Jan 1, 1970
+  
+return 0;
+}
