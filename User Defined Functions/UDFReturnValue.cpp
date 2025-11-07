31a14f3e7b64d367d54b92a4770bb17a852e27db
Elaine <eeguia@gbox.adnu.edu.ph>
2025-11-07 14:21:12 +0800

    Create UDFReturnValue.cpp

---
+++ User Defined Functions/UDFReturnValue.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int product( int a, int b){   //creation and declaration of function
+  return a*b; //operation to be done put on the function
+}
+int main() {
+  cout << " The product of 10 and 15 is " << product(10,15) << endl; //will perform the declared function 
+  
+    return 0;
+}
