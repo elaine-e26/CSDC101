ba9a296eff309e86ca9e4d2419c07a3236099d82
Elaine <eeguia@gbox.adnu.edu.ph>
2025-11-07 12:28:59 +0800

    Create PFOtherMathFunctions.cpp

---
+++ Predefined Functions/PFOtherMathFunctions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath> 
+
+using namespace std;
+
+int main (){
+  float n = 6.5;
+  cout << "round to the nearest integer " << round(n) << endl;
+  cout << "round up " << ceil(n) << endl;
+  cout << "round down " << floor(n) << endl;
+  cout << "The value of n is " << n << endl;
+  
+return 0;
+}
