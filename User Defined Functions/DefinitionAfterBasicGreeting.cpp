beece5cda511cecfbf1ce471ee880a609d69e4e4
Elaine <eeguia@gbox.adnu.edu.ph>
2025-11-07 13:38:09 +0800

    Create DefinitionAfterBasicGreeting.cpp

---
+++ User Defined Functions/DefinitionAfterBasicGreeting.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+void GoodMorning();
+void GoodEvening();
+
+int main(){
+
+  GoodMorning();
+  GoodEvening();
+return 0;
+}
+
+void GoodMorning(){     
+  cout << "Hi! Good morning!" << endl;
+}
+void GoodEvening(){
+  cout << "Hello! Good evening!" << endl;
+}
