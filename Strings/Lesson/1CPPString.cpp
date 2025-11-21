a573b23d58a92529d709caf4d3a7996a0aba3532
Elaine <eeguia@gbox.adnu.edu.ph>
2025-11-21 13:38:59 +0800

    Create 1CPPString.cpp

---
+++ Strings/Lesson/1CPPString.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+string superName = "Homelander"; //using string to declare words
+
+cout<< "The name of the superhero is: " << superName << endl; //prints the whole string
+
+return 0;
+}
+
+//-------------------------------//
+#include <iostream>
+using namespace std;
+
+int main (){
+
+string superName = "Homelander";
+
+cout<< "The name of the superhero is: " << superName << endl; //prints the whole string
+cout<< superName[0]<< endl; //prints each character from its index
+cout<< superName[1]<< endl;
+cout<< superName[2]<< endl;
+cout<< superName[3]<< endl;
+cout<< superName[4]<< endl;
+cout<< superName[5]<< endl;
+cout<< superName[6]<< endl;
+cout<< superName[7]<< endl;
+cout<< superName[8]<< endl;
+cout<< superName[9]<< endl;
+
+return 0;
+}
