562f7a47cbf42105415afaebff1c84c78714d9d4
Elaine <eeguia@gbox.adnu.edu.ph>
2025-11-23 02:05:43 +0800

    Update TempConverter_WithFunctions.cpp

--- Functions/FunctionsActivity/TempConverter_WithFunctions.cpp
+++ Functions/FunctionsActivity/TempConverter_WithFunctions.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
+using namespace std;
 
-int main(){
+float cToFahrenheit(float celsius){ //celsius is the variable name of the temperature value input within this function
+    return (celsius * 9.0 / 5.0) + 32.0;
+}
 
+float fToCelsius(float fahrenheit){ //fahrenheit is the variable name of the temperature value input within this function
+  return (fahrenheit - 32.0) * 5.0 / 9.0;
+}
 
-
-return 0;
+int main() {
+  int choice;
+  float temperature, result;
+  
+    cout<< "Choose a conversion type: " << endl;
+    cout<< "1. Celsius -> Fahrenheit" << endl;
+    cout<< "2. Fahrenheit -> Celsius" << endl;
+    cout<< "Enter choice: ";
+    cin>> choice;
+    cout << "\nEnter temperature value: " << endl;
+    cin>> temperature;
+    
+    if(choice == 1){
+        result = cToFahrenheit(temperature); //called function to perform
+        cout<< "Celsius -> Fahrenheit" << endl;
+        cout<< "Result: " << temperature << "°C -> " << result << "°F" << endl;
+    }
+    else if(choice == 2){
+        result = fToCelsius(temperature);
+        cout<< "Fahrenheit -> Celsius" << endl;
+        cout<< "Result: " << temperature << "°F -> " << result << "°C" << endl;
+    }
+    else
+        cout<< "Invalid choice. Please choose between 1 or 2 only.";
+        
+        
+    return 0;
 }
